7988cfbbb731a250cd1f2124e5f67bb4654d2807
Carsten Woelk <carsten.woelk@riotfamily.org>
2010-11-12 13:05:24 +0100

    Fixes compilation issue

--- src/org/riotfamily/example/cache/ClockController.java
+++ src/org/riotfamily/example/cache/ClockController.java
@@ -7,9 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller("clock")
-@Cache(ttl="30s")
 public class ClockController {
 
+	@Cache(ttl="30s")
 	@RequestMapping("/clock")
 	public void handleRequest(Writer out) throws Exception {
 		out.write(String.valueOf(System.currentTimeMillis()));
